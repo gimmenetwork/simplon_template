2ff255a1477277833ca7d8f32f345408f3b9076a
Tino Ehrich <tino@bigpun.me>
2016-12-02 13:36:23 +0100

    updated assets handling

--- src/Template.php
+++ src/Template.php
@@ -16,23 +16,28 @@ class Template
      * @var array
      */
     private $assetsCss = [];
-
     /**
      * @var array
      */
     private $assetsJs = [];
-
     /**
      * @var array
      */
     private $assetsCode = [];
+    /**
+     * @return array
+     */
+    public function getAssetsCss()
+    {
+        return $this->assetsCss;
+    }
 
     /**
      * @param array $pathAssets
      *
-     * @return bool
+     * @return Template
      */
-    public function setAssetsCss(array $pathAssets)
+    public function addMultipleAssetsCss(array $pathAssets)
     {
         foreach ($pathAssets as $blockId => $paths)
         {
@@ -42,14 +47,14 @@ class Template
             }
         }
 
-        return true;
+        return $this;
     }
 
     /**
      * @param string $pathAsset
      * @param string $blockId
      *
-     * @return bool
+     * @return Template
      */
     public function addAssetCss($pathAsset, $blockId = null)
     {
@@ -58,17 +63,25 @@ class Template
             $blockId = 'default';
         }
 
-        $this->assetsCss[$blockId][md5($pathAsset)] = '<link rel="stylesheet" href="' . $pathAsset . '">';
+        $this->assetsCss = $this->addAsset($this->assetsCss, $blockId, $pathAsset);
 
-        return true;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getAssetsJs()
+    {
+        return $this->assetsJs;
     }
 
     /**
      * @param array $pathAssets
      *
-     * @return bool
+     * @return Template
      */
-    public function setAssetsJs(array $pathAssets)
+    public function addMultipleAssetsJs(array $pathAssets)
     {
         foreach ($pathAssets as $blockId => $paths)
         {
@@ -78,14 +91,14 @@ class Template
             }
         }
 
-        return true;
+        return $this;
     }
 
     /**
      * @param string $pathAsset
      * @param string $blockId
      *
-     * @return bool
+     * @return Template
      */
     public function addAssetJs($pathAsset, $blockId = null)
     {
@@ -94,16 +107,42 @@ class Template
             $blockId = 'default';
         }
 
-        $this->assetsJs[$blockId][md5($pathAsset)] = '<script type="text/javascript" src="' . $pathAsset . '"></script>';
+        $this->assetsJs = $this->addAsset($this->assetsJs, $blockId, $pathAsset);
 
-        return true;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getAssetsCode()
+    {
+        return $this->assetsCode;
+    }
+
+    /**
+     * @param array $items
+     *
+     * @return Template
+     */
+    public function addMultipleAssetsCode(array $items)
+    {
+        foreach ($items as $blockId => $codes)
+        {
+            foreach ($codes as $code)
+            {
+                $this->addAssetCode($code, $blockId);
+            }
+        }
+
+        return $this;
     }
 
     /**
      * @param string $code
      * @param string $blockId
      *
-     * @return bool
+     * @return Template
      */
     public function addAssetCode($code, $blockId = null)
     {
@@ -112,9 +151,9 @@ class Template
             $blockId = 'default';
         }
 
-        $this->assetsCode[$blockId][] = $code;
+        $this->assetsCode = $this->addAsset($this->assetsCode, $blockId, $code);
 
-        return true;
+        return $this;
     }
 
     /**
@@ -124,6 +163,7 @@ class Template
      * @param bool $withFileExtension
      *
      * @return string
+     * @throws \Simplon\Mustache\MustacheException
      */
     public function renderMustache($pathTemplate, array $params = [], array $customerParsers = [], $withFileExtension = false)
     {
@@ -142,6 +182,7 @@ class Template
      * @param bool $withFileExtension
      *
      * @return string
+     * @throws \Simplon\Phtml\PhtmlException
      */
     public function renderPhtml($pathTemplate, array $params = [], $withFileExtension = false)
     {
@@ -152,6 +193,25 @@ class Template
         $template = Phtml::render($pathTemplate, $params, $withFileExtension ? '' : '.phtml');
 
         return (string)$template;
+    }
+
+    /**
+     * @param array $assets
+     * @param string $blockId
+     * @param string $item
+     *
+     * @return array
+     */
+    private function addAsset(array $assets, $blockId, $item)
+    {
+        if (empty($assets[$blockId]))
+        {
+            $assets[$blockId] = [];
+        }
+
+        $assets[$blockId][] = $item;
+
+        return $assets;
     }
 
     /**
@@ -179,14 +239,31 @@ class Template
     {
         $flatAssets = [];
 
+        $wrappers = [
+            'css'  => '<link rel="stylesheet" href="{item}">',
+            'js'   => '<script type="text/javascript" src="{item}"></script>',
+            'code' => '<script type="text/javascript">{item}</script>',
+        ];
+
         foreach ($blockAssets as $blockId => $assets)
         {
-            $code = "\n" . join("\n", $assets) . "\n";
+            $lines = [];
+
+            foreach ($assets as $item)
+            {
+                if (in_array($type, ['css', 'js'], true))
+                {
+                    $item = str_replace('{item}', $item, $wrappers[$type]);
+                }
+
+                $lines[] = $item;
+            }
+
+            $code = "\n" . implode("\n", $lines) . "\n";
 
             if ($isCode === true && strpos($code, '<script') === false)
             {
-                /** @noinspection BadExpressionStatementJS */
-                $code = str_replace('{code}', $code, '<script type="text/javascript">{code}</script>');
+                $code = str_replace('{item}', $code, $wrappers['code']);
             }
 
             $flatAssets[$type . ucfirst(strtolower($blockId))] = $code;
